bc3dd46c36cc65ef6610de9050bcb584addee462
Antonio Hernández <azkane7@gmail.com>
2022-05-27 17:13:25 -0500

    Adds game_id to SideStackerEvents

--- events.py
+++ events.py
@@ -5,29 +5,34 @@ class SideStackerEvent:
     """
     This base class represents an event of a sidestacker game
     """
+    def __init__(self, game_id: str):
+        self.game_id = game_id
     pass
 
 
 class PlayerConnected(SideStackerEvent):
-    def __init__(self, player: Literal['X', 'C'], turn_order: Literal[0, 1]):
+    def __init__(self, game_id: str, player: Literal['X', 'C'], turn_order: Literal[0, 1]):
+        super().__init__(game_id)
         self.player = player
         self.turn_order = turn_order
 
 
 class PlayerDisconnected(SideStackerEvent):
-    def __init__(self, player: Literal['X', 'C']):
+    def __init__(self, game_id: str, player: Literal['X', 'C']):
+        super().__init__(game_id)
         self.player = player
 
 
 class PlayerWon(SideStackerEvent):
-    def __init__(self, player: Literal['X', 'C']):
+    def __init__(self, game_id: str, player: Literal['X', 'C']):
+        super().__init__(game_id)
         self.player = player
 
 
 class PiecePlaced(SideStackerEvent):
-    def __init__(self, player: Literal['X', 'C'], row: int, side: Literal['L', 'R'], turn: int):
+    def __init__(self, game_id: str, player: Literal['X', 'C'], row: int, side: Literal['L', 'R'], turn: int):
+        super().__init__(game_id)
         self.player = player
         self.row = row
         self.side = side
         self.turn = turn
-
